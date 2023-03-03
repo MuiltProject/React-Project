a555a815e073bc873fa746db8f5a2bc4d7b018af
KIMSEI1124 <workju1124@gmail.com>
2023-03-03 13:21:29 +0900

    design: footer UI 구현

--- src/layouts/pages/index.jsx
+++ src/layouts/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 import * as S from "./index.styled";
 
 function Page({ children }) {
@@ -8,6 +9,7 @@ function Page({ children }) {
     <S.PageContainer>
       <Header />
       <S.Content>{children}</S.Content>
+      <Footer />
     </S.PageContainer>
   );
 }
