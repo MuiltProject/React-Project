f847fe64462862ff263579aca19462a86c297115
KIMSEI1124 <workju1124@gmail.com>
2023-03-03 18:02:58 +0900

    feat: NotFound 컴포넌트 생성
    - route를 사용하여 정상적이지 않은 URL을 NotFound 컴포넌트 호출

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import Details from "../pages/Details";
 import Join from "../pages/Join";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
+import NotFound from "../pages/NotFound";
 import Outfit from "../pages/Outfit";
 import Review from "../pages/Review";
 
@@ -19,6 +20,7 @@ function Routes() {
       <Route path={"/login"} element={<Login />} />
       <Route path={"/outfit"} element={<Outfit />} />
       <Route path={"/review"} element={<Review />} />
+      <Route path={"*"} element={<NotFound />} />
     </BrowserRoutes>
   );
 }
