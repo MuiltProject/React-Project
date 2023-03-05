079c3e7f7f8d7c3d7a0c6dfbb937e56dedfca5fd
KIMSEI1124 <workju1124@gmail.com>
2023-03-05 19:38:18 +0900

    feat: Suspense, React.lazy 도입

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import * as S from "./index.styled";
+
+function Loading() {
+  return (
+    <S.Container>
+      <p>로딩중입니다.</p>
+    </S.Container>
+  );
+}
+export default Loading;
