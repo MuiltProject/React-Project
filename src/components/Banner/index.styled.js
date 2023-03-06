cf50231cf102058aaed9db2d854287f8d14f95f0
KIMSEI1124 <workju1124@gmail.com>
2023-03-06 20:12:15 +0900

    fix: footer - 높이 고정
    - content의 최소 높이를 824px로 고정
    
    Resolved: #15

--- src/components/Banner/index.styled.js
+++ src/components/Banner/index.styled.js
@@ -1,10 +1,11 @@
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-  height: 3000px;
+  height: 100%;
 `;
 
 const Banner = styled.img`
+  display: block;
   width: 100%;
   height: auto;
 `;
