ea41f5126755f903f99a96d3ab1481970326650a
KIMSEI1124 <workju1124@gmail.com>
2023-03-06 18:42:58 +0900

    feat: Main - Import Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import * as S from "./index.styled";
+
+function Banner() {
+  return (
+    <S.Container>
+      <h1>배너 입니다.</h1>
+    </S.Container>
+  );
+}
+export default Banner;
