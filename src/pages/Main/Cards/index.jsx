1e02c9598dab2bc991cc5a545f2e64e04212eee1
KIMSEI1124 <workju1124@gmail.com>
2023-03-07 20:04:21 +0900

    desing: Main - Cards 영역 설정
    높이를 75vh로 설정함

---
+++ src/pages/Main/Cards/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import * as S from "./index.styled";
+
+function Cards() {
+  return (
+    <S.Container>
+      <h1>Cards</h1>
+    </S.Container>
+  );
+}
+export default Cards;
---
+++ src/pages/Main/Cards/index.styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+
+  height: 75vh;
+`;
+
+export { Container };
