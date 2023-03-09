77caef0f16702dbfc074f08a8b3ab2e57aaa89ab
KIMSEI1124 <workju1124@gmail.com>
2023-03-09 11:28:32 +0900

    feat: Main - Category의 사진에 링크 추가
    react-router-dom의 Link를 사용하여 연결함

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import * as S from "./index.styled";
+import Pic1 from "../../assets/Category/women.jpg";
 
 function Category() {
   return (
     <S.Container>
-      <h1> Shop By Category </h1>
+      <S.Wrapper>
+        <h1> Shop By Category </h1>
+      </S.Wrapper>
+      <S.Wrapper>
+        <Link to="/example/hats">
+          <S.Pic src={Pic1}></S.Pic>
+        </Link>
+        <Link to="/example/tops">
+          <S.Pic src={Pic1}></S.Pic>
+        </Link>
+        <Link to="example/bags">
+          <S.Pic src={Pic1}></S.Pic>
+        </Link>
+        <Link to="example/pants">
+          <S.Pic src={Pic1}></S.Pic>
+        </Link>
+      </S.Wrapper>
     </S.Container>
   );
 }
--- src/components/Category/index.styled.js
+++ src/components/Category/index.styled.js
@@ -4,8 +4,16 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
+
+const Wrapper = styled.div``;
 
 const Header = styled.h1``;
 
-export { Container, Header };
+const Pic = styled.img`
+  width: 25%;
+  height: 65vh;
+`;
+
+export { Container, Wrapper, Header, Pic };
