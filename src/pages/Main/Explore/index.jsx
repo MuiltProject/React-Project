61b296a3123df2a183203f1aa76e46b34a9977df
KIMSEI1124 <workju1124@gmail.com>
2023-03-09 15:01:06 +0900

    feat: Main - explore 링크 기능 구현
    - 홈페이지 외부 링크는 a 태그를 사용
    - 홈페이지 내부 링크는 react-router-dom의 Link 태그 사용

--- src/pages/Main/Explore/index.jsx
+++ src/pages/Main/Explore/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import * as S from "./index.styled";
 
@@ -6,6 +7,12 @@ function Explore() {
   return (
     <S.Container>
       <S.Header>Explore $(4팀)</S.Header>
+      <a href={"https://github.com/MultiProject23/React-Project"} target={"_blank"} rel="noreferrer">
+        github 저장소
+      </a>
+      <Link to={"/people"} target={"_blank"}>
+        피플
+      </Link>
     </S.Container>
   );
 }
--- src/pages/Main/Explore/index.styled.js
+++ src/pages/Main/Explore/index.styled.js
@@ -1,9 +1,18 @@
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-  margin-top: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  margin: 20vh 0;
 `;
 
-const Header = styled.h1``;
+const Wrapper = styled.div``;
+
+const Header = styled.h2`
+  font-weight: bold;
+`;
 
 export { Container, Header };
