8f11937a58f4b57a7a9dda351a5dd38c3cca9ec6
rrimm <119832853+rrimm@users.noreply.github.com>
2023-03-10 08:13:48 +0000

    Design: 연관상품 이미지 하단 텍스트 기능 추가

--- src/pages/Details/info/RelatedProduct.jsx
+++ src/pages/Details/info/RelatedProduct.jsx
@@ -9,8 +9,6 @@ import styled from "@emotion/styled";
 import { BsChevronLeft } from "react-icons/bs";
 import { BsChevronRight } from "react-icons/bs";
 
-
-
 const Button = styled.button`
   background-color: white;
   color: black;
@@ -19,19 +17,19 @@ const Button = styled.button`
   cursor: pointer;
   
 `;
-const ContainerStyle = {
-  width: "600px", 
-height: "530px", 
- 
-};
+
+const TextStyle = {
+  textAlign: "right"
+}
 
 function RelatedProduct() {
+
   const data = [
-    { image: c1 },
-    { image: c2 },
-    { image: c3 },
-    { image: c4 },
-    { image: c5 },
+    { image: c1, product: "코트1", price: "110,000원" },
+    { image: c2, product: "코트2", price: "120,000원" },
+    { image: c3, product: "코트3", price: "130,000원" },
+    { image: c4, product: "코트4", price: "140,000원" },
+    { image: c5, product: "코트5", price: "150,000원" },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -45,7 +43,7 @@ function RelatedProduct() {
       setCurrentIndex(data.length - numVisible);
     }
   };
-// 오른쪽 화살표 클릭 시 왼쪽 이미지 show
+// 오른쪽 화살표 클릭 시 오른쪽 이미지 show
   const handleNextClick = () => {
     if (currentIndex + numVisible < data.length) {
       setCurrentIndex(currentIndex + 1);
@@ -55,17 +53,18 @@ function RelatedProduct() {
   };
 
   const visibleData = data.slice(currentIndex, currentIndex + numVisible);
-
+// 연관상품을 carousel로 구현: 이미지 하단에 상품 이름과 가격 표시
   return (
     <div className="Related">
       <div style={{ textAlign: "center" }}>
         <div style={{ display: "flex", justifyContent: "center" }}>
           <Button onClick={handlePrevClick}><BsChevronLeft /></Button>
           {visibleData.map((item, index) => (
-            <div key={index} style={{ margin: "0 30px"}}>
-              <img src={item.image} alt="carousel-thumbnail" style={{ width: "250px", height: "300px"}} />
-            </div>
-          ))}
+  <div key={index} style={{ margin: "0 30px"}}>
+    <img src={item.image} alt="carousel-thumbnail" style={{ width: "200px", height: "250px"}} />
+    <p style={TextStyle}>{item.product}<br/>{item.price}</p>
+  </div>
+))}  
           <Button onClick={handleNextClick}><BsChevronRight /></Button>
         </div>
       </div>
