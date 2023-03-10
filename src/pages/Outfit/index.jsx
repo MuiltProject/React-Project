1f7b265fe888016b4645c039ad1a1dac182dc08a
KIMSEI1124 <workju1124@gmail.com>
2023-03-10 16:22:11 +0900

    design: [황성민] Outfit UI 설계

--- src/pages/Outfit/index.jsx
+++ src/pages/Outfit/index.jsx
@@ -1,9 +1,102 @@
 import React from "react";
+import "./index.styled.css";
+import outfit_image from "./image/outfit_image.jpeg";
+import outfit_image2 from "./image/outfit_image2.jpg";
+import outfit_image3 from "./image/outfit_image3.jpg";
+import outfit_image4 from "./image/outfit_image4.png";
+import search_image from "./image/search_image.png";
 
 function Outfit() {
   return (
-    <div>
-      <h1>Outfit</h1>
+    <div id="outfit">
+      <div>
+        <h1 id="outfit-title">Outfit</h1>
+      </div>
+      <div>
+        <section id="main">
+          <div className="image_box">
+            <img src={outfit_image} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image2} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image3} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image4} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image2} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image3} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image4} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image2} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image3} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="image_box">
+            <img src={outfit_image4} alt="alt" className="image" />
+            <p className="text">Outfit</p>
+            <p className="text">Style#000000</p>
+          </div>
+          <div className="bottom">
+            <table>
+              <tr>
+                <input type="text" placeholder="Search" className="search_bar" />
+                <button id="img_button">
+                  <img src={search_image} alt="alt" />
+                </button>
+              </tr>
+            </table>
+            <table>
+              <tr>
+                <div className="page_nation">
+                  <a href="/">1</a>
+                  <a href="/">2</a>
+                  <a href="/">3</a>
+                  <a href="/">4</a>
+                  <a href="/">5</a>
+                </div>
+              </tr>
+            </table>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
