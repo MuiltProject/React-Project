d58c17edefeea4800b3fb790d6a9c08a3e622445
KIMSEI1124 <workju1124@gmail.com>
2023-03-12 18:53:18 +0900

    feat: 검색한 상품이 있는 경우에만 제품을 출력
    if문을 사용하여 해결함

--- src/components/Product/Card/index.jsx
+++ src/components/Product/Card/index.jsx
@@ -3,20 +3,20 @@ import React from "react";
 import "./index.styled.css";
 import { Link } from "react-router-dom";
 
-// productId : 제품의 고유 번호
-// productImg : 제품의 대표 이미지
-// productName : 제품의 이름
-// productPrice : 제품의 가격
-function Card({ productId, productImg, productName, productPrice }) {
-  const formatPrice = Number(productPrice).toLocaleString();
+// id : 제품의 고유 번호
+// img : 제품의 대표 이미지
+// name : 제품의 이름
+// price : 제품의 가격
+function Card({ product }) {
+  const formatPrice = Number(product.price).toLocaleString();
   return (
     <div className="img-container">
-      <Link to={`/details/${productId}`}>
-        <img className="image" src={productImg} alt=""></img>
+      <Link to={`/details/${product.id}`}>
+        <img className="image" src={product.image} alt=""></img>
       </Link>
       <div>
         <div className="text-container">
-          <p className="text">{productName}</p>
+          <p className="text">{product.name}</p>
           <p className="text">{formatPrice}원</p>
         </div>
       </div>
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -2,23 +2,37 @@ import React from "react";
 import "./index.styled.css";
 import Card from "../../components/Product/Card";
 
+import Json from "../../data/product/data.json";
+import NotFound from "../../components/Product/NotFound";
+
 // 제품의 고유 번호, 대표 이미지, 태그1, 태그2, 제품 이름, 가격
 const Product = () => {
-  return (
-    <div className="Container">
-      <div className="TextBox">
-        <h1>신상품</h1>
+  const total = Json.count;
+
+  if (total === 0) {
+    return (
+      <div className="Container">
+        <div className="TextBox">
+          <h1>Products</h1>
+        </div>
+        <div className="Wrapper">
+          <NotFound />
+        </div>
       </div>
-      <div className="Wrapper">
-        <Card productId={"1"} productImg={"img/03.jpg"} productName={"데님 자켓"} productPrice={218000} />
-        <Card productId={"2"} productImg={"img/04.jpg"} productName={"데님 자켓"} productPrice={118000} />
-        <Card productId={"3"} productImg={"img/05.jpg"} productName={"가죽 자켓"} productPrice={1218000} />
-        <Card productId={"4"} productImg={"img/06.jpg"} productName={"데님 자켓"} productPrice={98000} />
-        <Card productId={"5"} productImg={"img/07.jpg"} productName={"롱 무엇"} productPrice={508000} />
-        <Card productId={"6"} productImg={"img/08.jpg"} productName={"후드티"} productPrice={88000} />
+    );
+  } else {
+    const cards = Json.products.map((product) => {
+      return <Card key={product.id} product={product} />;
+    });
+    return (
+      <div className="Container">
+        <div className="TextBox">
+          <h1>Products</h1>
+        </div>
+        <div className="Wrapper">{cards}</div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Product;
