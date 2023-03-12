2c4b5b6a78a1c01d1834edfe8e8d41a729f1e3b4
KIMSEI1124 <workju1124@gmail.com>
2023-03-12 19:23:33 +0900

    feat: 검색한 상품의 개수 출력
    카운트 쿼리를 받았을 때 상품의 개수를 출력
    
    Related to: #52

---
+++ src/components/Product/Nav/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import * as S from "./index.styled";
+
+function Nav() {
+  return (
+    <S.Container>
+      <p />
+    </S.Container>
+  );
+}
+export default Nav;
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -7,32 +7,25 @@ import NotFound from "../../components/Product/NotFound";
 
 // 제품의 고유 번호, 대표 이미지, 태그1, 태그2, 제품 이름, 가격
 const Product = () => {
-  const total = Json.count;
+  // TODO: 추후 axios로 API 호출하는 로직 필요
 
-  if (total === 0) {
-    return (
-      <div className="Container">
-        <div className="TextBox">
-          <h1>Products</h1>
-        </div>
-        <div className="Wrapper">
+  return (
+    <div className="Container">
+      <div className="TextBox">
+        <h1>Products</h1>
+      </div>
+      <p className="Text">{Json.count} 상품</p>
+      <div className="Wrapper">
+        {Json.count === 0 ? (
           <NotFound />
-        </div>
+        ) : (
+          Json.products.map((product) => {
+            return <Card key={product.id} product={product} />;
+          })
+        )}
       </div>
-    );
-  } else {
-    const cards = Json.products.map((product) => {
-      return <Card key={product.id} product={product} />;
-    });
-    return (
-      <div className="Container">
-        <div className="TextBox">
-          <h1>Products</h1>
-        </div>
-        <div className="Wrapper">{cards}</div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Product;
