6f12f94b49da15040ba28015b44ce6dc1a624890
KIMSEI1124 <workju1124@gmail.com>
2023-03-13 21:09:37 +0900

    design: 카테고리 사진 변경

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 import * as S from "./index.styled";
-import Pic1 from "../../assets/Category/women.jpg";
+import Total from "../../assets/Category/total.jpg";
+import Bag from "../../assets/Category/bag.jpg";
+import Basic from "../../assets/Category/basic.jpg";
+import Hat from "../../assets/Category/hat.jpg";
+import Outer from "../../assets/Category/outer.jpg";
+import Pant from "../../assets/Category/pant.jpg";
+import Shoes from "../../assets/Category/shoes.jpg";
+import Top from "../../assets/Category/top.png";
 
 function Category() {
   // TODO: 이미지 변경 필요
@@ -12,37 +19,37 @@ function Category() {
       <S.Section>
         <S.StyledLink to="/product">
           <S.Text>전체</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Total}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=hat">
           <S.Text>모자</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Hat}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=outer">
           <S.Text>아우터</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Outer}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=top">
           <S.Text>상의</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Top}></S.Pic>
         </S.StyledLink>
       </S.Section>
       <S.Section>
         <S.StyledLink to="/product?category=bag">
           <S.Text>가방</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Bag}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=pant">
           <S.Text>하의</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Pant}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=shoes">
           <S.Text>신발</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Shoes}></S.Pic>
         </S.StyledLink>
         <S.StyledLink to="/product?category=basic">
           <S.Text>베이직</S.Text>
-          <S.Pic src={Pic1}></S.Pic>
+          <S.Pic src={Basic}></S.Pic>
         </S.StyledLink>
       </S.Section>
     </S.Container>
