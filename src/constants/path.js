706b18eb674695ff546a13bfee7b2ba0535926a5
KIMSEI1124 <workju1124@gmail.com>
2023-03-14 21:42:00 +0900

    refactor: BROWSER_PATH 분리

---
+++ src/constants/path.js
@@ -0,0 +1,19 @@
+const BROWSER_PATH = {
+  BASE: "/",
+  CART: "/cart",
+  DETAILS: "/details",
+  JOIN: "/join",
+  LOGIN: "/login",
+  MY: "/my",
+  OUTFIT: "/outfit",
+  PRODUCT: "/product",
+  REVIEW: "/review",
+};
+
+const API_PATH = {
+  MAIN: {
+    HEADER: "http:localhost:4000/my",
+  },
+};
+
+export { BROWSER_PATH, API_PATH };
